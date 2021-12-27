47e38d88c67e7fb1fc3b34cbc85f4c2f8cdb6c0f
Den Wolper <denwolper@MacBook-Pro-Den.local>
2021-12-27 13:49:33 +0200

    checkhealth added

--- MySms2Sasha.ino
+++ MySms2Sasha.ino
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 #define CODESMS "MISH"
+#define CODEHEALTH "VOLT"
+#define PHRASE "Hello, Dude! Temp="
+#define HPHRASE "Hello, Dude! Volt="
 
  char incomingByte; 
  String inputString;
@@ -34,24 +37,26 @@ void setup() {
  while (!Serial) ; 
  Serial.println("SHT31 test");
  sht20.initSHT20(); 
- delay(100);
+ delay(200);
  sht20.checkSHT20();
  delay(200); 
  Serial.println("AltSoftSerial Test Begin");
  altSerial.begin(9600);
- while(!altSerial.available()){               // Init SIM800L
+ while(!altSerial.available()){               
     altSerial.println("AT");                   
     delay(1000);                              
     Serial.println("Connecting…");             
   }
   Serial.println("Connected!");               
   delay(20000); 
-  altSerial.println("AT+CMGF=1");             // Send AT+CMGF=1
+  altSerial.println("AT+CMGF=1");    //Text mode         
   delay(1000);                                 
-  altSerial.println("AT+CNMI=1,2,0,0,0");     // Modes AT+CNMI=1,2,0,0,0
+  altSerial.println("AT+CNMI=1,2,0,0,1");   //Format read  
   delay(1000);                                 
-  altSerial.println("AT+CMGL=\"REC UNREAD\""); 
+  altSerial.println("AT+CMGDA=\"DEL ALL\"");  //Clear untread
   delay(1000);  
+  altSerial.println("AT+GSMBUSY=1");  //No call
+  delay(1000);
 }
 
 
@@ -68,30 +73,50 @@ int parseNum(char * s1){
 }
 
 
-void loop() {   
-   if(altSerial.available()){   
-      t = sht20.readTemperature();             
-      delay(200);                             
+void readData(){
+      inputString="";
       while(altSerial.available()){            
          incomingByte = altSerial.read();          
          inputString += incomingByte;              
       }   
-       delay(50);                                
+      delay(200);
+}
+
+
+void loop() {   
+   if(altSerial.available()){   
+       t = sht20.readTemperature();             
+       delay(200);                             
+       readData();   
+       //Case1 (command to measure)         
        if (inputString.indexOf(CODESMS) > -1)  {   
           Serial.println(inputString);
-          int res=parseNum(inputString.c_str());
 
+          int res=parseNum(inputString.c_str());
           if (res) {
              number=String(outnum);
              Serial.println(number);
-             sms(String("Hello, Mother Fucker! Temp="+String(round(t))), String(number)); 
+             sms(String(PHRASE+String(round(t))), String(number)); 
           }
-          delay(50);
        }
+       //Case2 (HealthChech)
+       if (inputString.indexOf(CODEHEALTH) > -1)  {   
+          Serial.println(inputString);
+           
+          int res=parseNum(inputString.c_str());
+          if (res) {
+             number=String(outnum);
+             Serial.println(number);
+             altSerial.println("AT+CBC");  //Check Voltage  
+             delay(1000);
+             readData();
+             sms(String(HPHRASE+inputString, String(number));
+          } 
+       }
+       //Clear SMSs and buffer
        if (inputString.indexOf("OK") == -1){    
         altSerial.println("AT+CMGDA=\"DEL ALL\"");      
         delay(1000);
        }     
-       inputString = "";
     }
 }
