3b7e7ddbebfb874a8e8864a26967aeecc12e90e9
Den Wolper <denwolper@MacBook-Pro-Den.local>
2021-12-29 00:18:58 +0200

    buffer optimized

--- MySms2Sasha.ino
+++ MySms2Sasha.ino
@@ -3,29 +3,41 @@
 #include <Wire.h>
 #include <stdio.h>
 #include <string.h>
+
 #define CODESMS "MISH"
 #define CODEHEALTH "VOLT"
 #define PHRASE "Hello, Dude! Temp(C)="
 #define HPHRASE "Hello, Dude! Power(mV)="
+#define VOLTSIGN "+CBC:"
+#define NUMSIGN "+380"
 #define BUFFSIZE 500
 #define TIMEOUT 10000
+
+//#define DEDUG
+#ifdef DEDUG
+#define LOG_ON(x,y) debugLog(x,y)
+#else
+#define LOG_ON(x,y)
+#endif
+
+
 
 
  AltSoftSerial altSerial;
  DFRobot_SHT20 sht20;
- const char * pat_num="+380";
- const char * pat_volt="+CBC:";
  char outnum [15];
  char outvolt [15];
  char incomingByte; 
  char inputString [BUFFSIZE+1];
  float t;
 
- 
 
- void sms(String text, String phone)  
+ 
+ void sms(String text, const char * phone)  
  {
-   altSerial.println("AT+CMGS=\"" + phone + "\"");
+   char * phones="AT+CMGS=\"+380663539598\"\0";
+   memcpy(phones+9, phone, 13);
+   altSerial.println(phones);
    waitOK();
    readData();
    altSerial.print(text);
@@ -33,11 +45,58 @@
    altSerial.print((char)26);
    waitOK();
    readData();
-   #ifdef DEBUG
-    Serial.println("SMS sended");
-   #endif
-   
+   LOG_ON("SMS sended to: ", phones); 
  } 
+
+
+int parseNum(const char * s1){
+  char * s;
+  char number [14];
+  s = strstr(s1, "+CMT:");
+  if (s) s = strstr(s, NUMSIGN);
+  if(s) {
+    strncpy(number, s, 14);
+    snprintf (outnum, 14, "%s", number);
+    return 1;
+  }
+  else return 0;
+}
+
+
+int parseVolt(const char * s1){
+  char * s, * e;
+  char number [14];
+  s = strstr(s1, VOLTSIGN);
+  if (s) s = strstr(s+1, ",");
+  if (s) s = strstr(s+1, ",");
+  if (s) e = strstr(s+1, "\n");
+  if (s && e && (byte) (e-s)>10) e = s + 10;
+  if(s && e) {
+    strncpy(number, s+1, (byte) (e-s+1));
+    snprintf (outvolt,  (byte) (e-s+1), "%s", number);
+    return 1;
+  }
+  else return 0;
+}
+
+
+void readData(){
+      int is=0;
+      while(altSerial.available() && is<BUFFSIZE){            
+         incomingByte = altSerial.read();          
+         inputString[is++] = incomingByte;              
+      }  
+      inputString[is]='\0';
+}
+
+
+void waitOK(){
+  unsigned long _timeout = millis() + TIMEOUT;
+  while (!altSerial.available() && millis() < _timeout)  {};  
+  delay(300);                                         
+}
+
+
 
 void setup() {
  Serial.begin(9600);
@@ -76,111 +135,54 @@ void setup() {
 }
 
 
-int parseNum(const char * s1){
-  char * s;
-  char number [14];
-  s = strstr(s1, "+CMT:");
-  if (s) s = strstr(s, pat_num);
-  if(s) {
-    strncpy(number, s, 14);
-    snprintf (outnum, 14, "%s", number);
-    return 1;
-  }
-  else return 0;
-}
-
-
-int parseVolt(const char * s1){
-  char * s, * e;
-  char number [14];
-  s = strstr(s1, pat_volt);
-  if (s) s = strstr(s+1, ",");
-  if (s) s = strstr(s+1, ",");
-  if (s) e = strstr(s+1, "\n");
-  if (s && e && (byte) (e-s)>10) e = s + 10;
-  if(s && e) {
-    strncpy(number, s+1, (byte) (e-s+1));
-    snprintf (outvolt,  (byte) (e-s+1), "%s", number);
-    return 1;
-  }
-  else return 0;
-}
-
-
-void readData(){
-      int is=0;
-      while(altSerial.available() && is<BUFFSIZE){            
-         incomingByte = altSerial.read();          
-         inputString[is++] = incomingByte;              
-      }  
-      inputString[is]='\0';
-}
-
-
-void waitOK(){
-  unsigned long _timeout = millis() + TIMEOUT;
-  while (!altSerial.available() && millis() < _timeout)  {};  
-  delay(300);                                         
-}
 
 
 void loop() {   
    if(altSerial.available()){   
-       t = sht20.readTemperature();  
        waitOK();                                     
-       readData();   
+       readData();  
+        
        //Case1 (command to measure)         
        if (strstr(inputString, CODESMS))  { 
-          #ifdef DEBUG
-           Serial.print("input: ");  
-           Serial.write(inputString, strlen(inputString));
-           Serial.print('\n');
-          #endif
-          
+          LOG_ON("input: ", inputString);  
           int res=parseNum(inputString);
           if (res) {
-            #ifdef DEBUG
-             Serial.print("number: ");
-             Serial.write(outnum, strlen(outnum));
-             Serial.print('\n');
-            #endif       
-             sms(String(PHRASE+String(round(t))), String(outnum)); 
+            LOG_ON("number: ", outnum);  
+            t = sht20.readTemperature();      
+            sms(String(PHRASE+String(round(t))), outnum); 
           }
        }
+       
        //Case2 (HealthChech)
        if (strstr(inputString, CODEHEALTH))  {   
-         #ifdef DEBUG
-          Serial.print("input: ");  
-          Serial.write(inputString, strlen(inputString));
-          Serial.print('\n');
-         #endif
-           
+          LOG_ON("input: ", inputString);  
           int res=parseNum(inputString);
           if (res) {
-            #ifdef DEBUG
-             Serial.print("number: ");
-             Serial.write(outnum, strlen(outnum));
-             Serial.print('\n');
-            #endif        
+             LOG_ON("number: ", outnum);      
              altSerial.println("AT+CBC");  //Check Voltage  
              waitOK();
              readData();
              int res=parseVolt(inputString);
              if (res) {
-             #ifdef DEBUG
-              Serial.print("voltage: ");
-              Serial.write(outvolt, strlen(outvolt));
-              Serial.print('\n');
-             #endif  
-              sms(HPHRASE+String(outvolt), String(outnum));
+             LOG_ON("voltage: ", outvolt);
+             sms(HPHRASE+String(outvolt), outnum);
              }
           } 
        }
-       //Clear SMSs and buffer
+       
+       //Default case (Clear SMSs and buffer)
        if (!strstr(inputString, "OK")){    
         altSerial.println("AT+CMGDA=\"DEL ALL\"");      
         waitOK(); 
         readData();
        }     
     }
+}
+
+
+
+void debugLog(const char * s, const char * c){
+           Serial.write(s, strlen(s));
+           Serial.write(c, strlen(c));
+           Serial.print('\n');
 }
